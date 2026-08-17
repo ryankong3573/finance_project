aac92582c0b160e025ae66ae56d4db9b1b631f38
Ryan Kong <ryankong3573@gmail.com>
2026-08-17 23:50:29 +0800

    added enter password function

--- headers.cpp
+++ headers.cpp
@@ -11,4 +11,25 @@ int receive_input()
 
     return input - 1;
 }
+string create_password()
+{
+    bool password_confirmed = false;
+        string password1, password2;
+        while(!password_confirmed)
+        {
+            cout<<"Enter Password: ";
+            cin>>password1;
+            cout<<"Confirm Password: ";
+            cin>>password2;
 
+            if(password1==password2)
+            {
+                password_confirmed = true;
+            }
+            else
+            {
+                cout<<"Password is not the same (try again)"<<endl;
+            }
+        }
+    return password1;
+}
